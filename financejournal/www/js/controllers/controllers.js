c93c089c0a6baf1238615fd915ff705425fc86ff
Sixtey7 <sixtey7@gmail.com>
2016-07-30 23:08:41 -0400

    make the edit/create form more generic

--- financejournal/www/js/controllers/controllers.js
+++ financejournal/www/js/controllers/controllers.js
@@ -30,24 +30,41 @@ angular.module('financeJournal.controllers', [])
     };
 
     /******************************
+     * Generic Form Crap
+     */
+    $scope.submitEntryForm = function() {
+      if ($scope.formEditMode) {
+        $log.debug('Edit Form Submitted');
+        $scope.commitEditTransaction();
+      }
+      else {
+        $log.debug('Create Form Submitted');
+        newTransaction();
+      }
+    };
+
+
+    $scope.cancelTransactionForm = function() {
+      $log.debug('Cancel the transaction form');
+      $scope.formEntry = {};
+      $scope.modal.hide();
+    };
+
+    /******************************
      * Transaction Add Crap
      */
      $scope.newTransaction = function() {
       $log.debug('New Transaction Is Running!');
       $scope.formEntry = {};
+      $scope.formEditMode = false;
 
       $scope.modal.show();
       document.getElementById('datePicker').valueAsDate = new Date();
 
      };
 
-    $scope.cancelAddNewTransaction = function() {
-      $log.debug('Cancel adding new transaction');
-      $scope.modal.hide();
-    };
-
     $scope.addNewTransaction = function() {
-      $log.debug('Posted: ' + JSON.stringify(this.entry));
+      $log.debug('Posted new entry: ' + JSON.stringify(this.formEntry));
 
       //Posted: {"source":"Source","amount":1209,"date":"2016-07-22T06:00:00.000Z","estimate":true,"planned":true,"notes":"Notes"}
       var entryToAdd = {
@@ -80,7 +97,12 @@ angular.module('financeJournal.controllers', [])
           notes : entryToEdit.notes
         };
 
+        $scope.formEditMode = true;
         $scope.modal.show();
+      };
+
+      $scope.commitEditTransaction = function() {
+        $log.debug('Posted to edit: ' + JSON.stringify(this.formEntry));
       };
 
       $scope.deleteEntry = function(entryToDelete) {
